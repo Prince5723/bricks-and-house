efd53b85fa32f6f37f08750f5563d07a0cd2f432
Prince5723 <57prince23@gmail.com>
2025-03-21 00:45:25 +0530

    Project init and navbar

---
+++ src/components/blocks/shadcnblocks-com-navbar1.tsx
@@ -0,0 +1,185 @@
+import { Book, Menu, Sunset, Trees, Zap } from "lucide-react";
+import * as React from 'react';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+interface MenuItem {
+  title: string;
+  url: string;
+  description?: string;
+  icon?: React.ReactElement;
+  items?: MenuItem[];
+}
+
+interface Navbar1Props {
+  logo?: {
+    url: string;
+    src: string;
+    alt: string;
+    title: string;
+  };
+  menu?: MenuItem[];
+  mobileExtraLinks?: {
+    name: string;
+    url: string;
+  }[];
+  auth?: {
+    login: {
+      text: string;
+      url: string;
+    };
+    signup: {
+      text: string;
+      url: string;
+    };
+  };
+}
+
+const Navbar1 = ({
+  logo = {
+    url: "/",
+    src: "/bricks-and-house-logo.webp",
+    alt: "logo",
+    title: "",
+  },
+  menu = [
+    { title: "Home", url: "#" },
+    { title: "Our Projects", url: "#" },
+    { title: "Floor Plans", url: "#" },
+    { title: "Cost Estimator", url: "#" },
+    { title: "About Us", url: "#" },
+    { title: "Contact Us", url: "#" },
+  ],
+  mobileExtraLinks = [
+    { name: "Press", url: "#" },
+    { name: "Contact", url: "#" },
+    { name: "Imprint", url: "#" },
+    { name: "Sitemap", url: "#" },
+  ],
+  auth = {
+    login: { text: "Log in", url: "#" },
+    signup: { text: "Sign up", url: "#" },
+  },
+}: Navbar1Props) => {
+  return (
+    <section className="px-8 py-4">
+      <div className="container">
+        <nav className="hidden justify-between lg:flex">
+          <div className="flex items-center gap-6">
+            <a href={logo.url} className="flex items-center gap-2">
+              <img src={logo.src} className="w-48" alt={logo.alt} />
+              <span className="text-lg font-semibold">{logo.title}</span>
+            </a>
+            <div className="flex items-center">
+              <NavigationMenu>
+                <NavigationMenuList>
+                  {menu.map((item) => renderMenuItem(item))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Button asChild variant="outline" size="sm">
+              <a href={auth.login.url}>{auth.login.text}</a>
+            </Button>
+            <Button asChild size="sm">
+              <a href={auth.signup.url}>{auth.signup.text}</a>
+            </Button>
+          </div>
+        </nav>
+        <div className="block lg:hidden">
+          <div className="flex items-center justify-between">
+            <a href={logo.url} className="flex items-center gap-2">
+              <img src={logo.src} className="w-8" alt={logo.alt} />
+              <span className="text-lg font-semibold">{logo.title}</span>
+            </a>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Menu className="size-4" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="overflow-y-auto">
+                <SheetHeader>
+                  <SheetTitle>
+                    <a href={logo.url} className="flex items-center gap-2">
+                      <img src={logo.src} className="w-8" alt={logo.alt} />
+                      <span className="text-lg font-semibold">
+                        {logo.title}
+                      </span>
+                    </a>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="my-6 flex flex-col gap-6">
+                  <div className="flex flex-col gap-4">
+                    {menu.map((item, index) => (
+                      <a key={index} href={item.url} className="font-semibold">
+                        {item.title}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="border-t py-4">
+                    <div className="grid grid-cols-2 justify-start">
+                      {mobileExtraLinks.map((link, idx) => (
+                        <a
+                          key={idx}
+                          className="inline-flex h-10 items-center gap-2 whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
+                          href={link.url}
+                        >
+                          {link.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <Button asChild variant="outline">
+                      <a href={auth.login.url}>{auth.login.text}</a>
+                    </Button>
+                    <Button asChild>
+                      <a href={auth.signup.url}>{auth.signup.text}</a>
+                    </Button>
+                  </div>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const renderMenuItem = (item: MenuItem) => {
+  return (
+    <a
+      key={item.title}
+      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-md font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
+      href={item.url}
+    >
+      {item.title}
+    </a>
+  );
+};
+
+export { Navbar1 };
