cbb26c39fdd6ed8f4d38ec7ca068c5fdfe028eed
Prince5723 <57prince23@gmail.com>
2025-05-19 03:06:11 +0530

    changed pricing sections

--- src/components/pricing-section.tsx
+++ src/components/pricing-section.tsx
@@ -3,16 +3,16 @@ import PricingCard from './blocks/pricing-section';
 const pricingData = {
   lilly: {
     title: 'ROSE',
-    price: '₹ 1900/sq.ft (incl. GST)',
+    price: '₹ 1950/sq.ft',
     color: 'blue' as const,
     features: [
       {
         title: 'Designs & Drawings',
         details: [
-          'Designs & Diagram',
+          'Designs & Drawings',
           'Architectural Layout 2D',
           'Basic Elevation Structural Design',
-          'Elevation 30',
+          'Elevation 3D',
         ]
       },
       {
@@ -24,28 +24,34 @@ const pricingData = {
           'Brick - 7.5 N/cm²',
           'Block - AAC Block, 300m, 200mm',
           'Brick - 7.5 N/cm²',
-          'RCC, Desigen mix 120/m25%',
-          'PCC - 47.5',
-          'Ceiling height - 11 FT, (Finish floored to Roof Level)'
-
+          'RCC, Desigen mix 20, 25, m',
+          'PCC - 7.5 grade/False Ceiling',
+          'Ceiling height - 11 FT, (Finish floored to Roof Level)',
+          'Water proofing - Footings',
+          'Staircase/Balcony - MS Railing',
+          'Anti termite treatment',
         ]
       },
       {
         title: 'Kitchen',
         details: [
           'Ceramic wall dado, upto Rs. 40 per sqft, Main sink faucet upto Rs 1500',
-          'Any othe faucet or Accessories 11 -ISI marked',
-          'Kitchen Sink- S.S. Worth Rs 4000'
+          'Any other faucet or Accessories upto Rs.10000 - ISI marked',
+          'Kitchen Sink- S.S. Worth Rs 4000',
+          'Modular Kitchen',
+          'Chimney',
+          'Water Proofing - Sink Area',
         ]
       },
       {
         title: 'Bathroom',
         details: [
-          'Ceremic wall dado upto 7" height Rs-40/Sqft',
-          'Sanitary ware up to 30,000 per 100 sqft. of Cera or Hindware make',
-          'CPVC pipe APL Apollo or Equivalent',
-          'Bathroom doors - Waterproof flush door'
-
+          'Ceramic tiles upto 7" height Rs-65/Sqft',
+          'Sanitary ware up to 30,000, mark ISI',
+          'CPVC pipe fitting Supreme/Astral + Geyser (15 Ltr)',
+          'Bathroom doors - Aluminium frame with Acralic',
+          'Water Proofing - Sunken Waterproofing',
+          'Overhead water tank 2000 Ltr',
         ]
       },
       {
@@ -61,8 +67,8 @@ const pricingData = {
       {
         title: 'Painting',
         details: [
-          'JK putti + Tractor Emulsion or quivalent',
-          'External Asian primer + Ace emulsion paint or equivalent'
+          'Internal painting - Wall PDP/Primer/Chaak Mitti, Paint Emulsion(Asian/Berger), Door Frame - Deco. Paint, Texture Paint as decided',
+          'External painting - JK putty/Sakarni putty, Waterproof Primer, Weather Shield paint - Asian/Berger',
         ]
       },
       {
@@ -71,8 +77,10 @@ const pricingData = {
           'Living & Dinning Flooring - Tiles of - value up to Rs 60 per sqft',
           'Rooms & Kitchen flooring - Tiles of Rs.50 per sqft',
           'Balcony, open area flooring - Antiskide tiles of Rs. 40 per sqft',
-          'Stair case flooring - Granite of value Rs75 per sqft',
-          'Parking tiles- Antiskid tiles or checked tiles / paver block'
+          'Stair case flooring - Granite of value Rs.150 per sqft',
+          'Parking tiles- Antiskid tiles or checked tiles / paver block',
+          'Roof flooring - Moracco tiles upto Rs.125 per sqft',
+          'Water proofing - Roof, Balcony, Agent - Pidilite(LW/URP)',
 
         ]
       },
@@ -80,14 +88,26 @@ const pricingData = {
         title: 'Electrical',
         details: [
           'Wire - fire proof wire by Finolex',
-          'Switches & sockets - legards or equivalent'
+          'Switches & sockets - legards or equivalent',
+          'Lights',
+          'Fans',
+          'Earthing',
+          'Chandeliers',
+          
+        ]
+      },
+      {
+        title: 'Smart Homes',
+        details: [
+          'Wi-Fi',
+          'Heat Pump'
         ]
       }
     ]
   },
   rose: {
     title: 'ORCHID',
-    price: '₹ 2200/sq.ft (incl. GST)',
+    price: '₹ 2000/sq.ft',
     color: 'orange' as const,
     features: [
       {
@@ -95,8 +115,7 @@ const pricingData = {
         details: [
           'Architectural layout - 2D. Basic elevation',
           'Structural Design 3D elevation',
-          'Electrical Drawings',
-          'Plumbing Drawings'
+          'All Services Drawings - Electrical, Plumbing, AC, HVAC or VPV',
 
         ]
       },
@@ -108,7 +127,9 @@ const pricingData = {
           'Aggregates - 20, 10, 40 mm',
           'Bricks. - 7.5 N/cm², AAC Blocks',
           'RCC Design Mix - All approved by Architect',
-          'Celing height 11 feet  ( FFL TO FFL )'
+          'Celing height 11 feet  ( FFL TO FFL )',
+          'False Ceiling',
+          'Staircase/Balcony - MS Railing/Glass Railing',
 
         ]
       },
@@ -118,7 +139,11 @@ const pricingData = {
           'Ceramic wall dado upto Rs. 80 sqft',
           'Main Sink. faucet - upto Rs- 4000/-',
           'Any of the faucet or Accessores ISI Mark',
-          'Kitchen counter of Granite finish'
+          'Kitchen counter of Granite finish',
+          'Modular Kitchen',
+          'Chimney',
+          'Fridge - 236 Ltr (Double Door)',
+          'Water Proofing - Sink LW/URP',
 
         ]
       },
@@ -129,7 +154,9 @@ const pricingData = {
           'Sanitaryware & cpvc fitting up to Rs. 45000 per sqft of Jaguar make',
           'CPVC pipe - Apl Apollo, or similar',
           'Doors - waterproof flush doors',
-          'Mirror, soap dish, towel rail, worth Rs. 7000 & water heater'
+          'Heat grill',
+          'Overhead water tank 2000 Ltr',
+          'Sunken Waterproofing - Agent (LW/URP)',
 
         ]
       },
@@ -137,27 +164,30 @@ const pricingData = {
         title: 'Doors & Windows',
         details: [
           'UPVC windows (3 Track, 5mm glass thickness)',
-          'Teak Door frame with Teak doors worth - Rs. 15000/-',
-          'Internal doors flush doors (32mm) with laminates, (Door frame- Marandi or Sal)'
+          'Internal doors flush doors and main door (32mm) with Veneer, (Door frame - Marandi or Sal)',
+          'Wardrobe Wooden / SS',
+          'TV wall Panelling',
 
         ]
       },
       {
         title: 'Painting',
         details: [
-          'Internal painting',
-          'Emulsion, JK pulty, Apcalite premium',
-          'External - Asian Apex Emulsion'
+          'Internal painting - Wall POP, Primer, Chaak Primer, Royal Emulsion for Ceiling, Velvet touch for walls, Door frame - Deco. Paint, Texture Paint as decided',
+          'External - JK putty, Deluxe weather shield paint',
 
         ]
       },
       {
         title: 'Flooring',
         details: [
-          'Living & Dinning flooring - Tiles or granite or marble up to Rs 120 per sqft',
+          'Living & Dinning flooring - Tiles or granite or marble up to Rs 150 per sqft',
           'Rooms, Kitchen,Living Area,Balcony & Open Area- Rs.100 per sqft. anti skid tiles of value Rs.80 per sqft',
-          'Staircase, Flooring Granite upto Rs 100 per sqft.',
-          'Parking Anti Skid tiles / Kota Stone / Paver Block'
+          'Staircase, Flooring Granite upto Rs 150 per sqft.',
+          'Parking Anti Skid tiles / Kota Stone / Paver Block',
+          'Balcony - Moracco tiles',
+          'Roof - Moracco tiles',
+          'Water proofing - URP (Agent)',
 
         ]
       },
@@ -165,25 +195,41 @@ const pricingData = {
         title: 'Electrical',
         details: [
           'Fire proof wire flonex switches',
-          'Sockets - Legrard ups wiring'
+          'Sockets - Legrard ups wiring',
+          'Lights',
+          'Fans',
+          'Chandeliers',
+          'Earthing'
+          
+        ]
+      },
+      {
+        title: 'Smart Homes',
+        details: [
+          'Wi-Fi CAT-6',
+          'CCTV Camera',
+          'Main Gate lock - Smart Lock',
+          'Centralized RO fitting',
+          'Heat pump',
+          'Shoftner'
+          
         ]
       }
     ]
   },
   orchid: {
     title: 'LILY',
-    price: '₹ 2600/sq.ft (incl. GST)',
+    price: '₹ 3000/sq.ft',
     color: 'gold' as const,
     features: [
       {
         title: 'Designs & Drawings',
         details: [
-          'Architectural layout 2D',
-          'Basic Elevation  Structural Design',
+          'Architectural layout 3D',
+          'Elevation  Structural Design',
           '3D Elevations',
-          'Electrical Drawing',
-          'Plumbing Drawing',
-          'Furniture Plan'
+          'All Services Drawings - Electrical, Plumbing, Furniture, Centralized AC',
+          'Interior Drawings '
         ]
       },
       {
@@ -194,59 +240,95 @@ const pricingData = {
           'Aggregate - 20, 10, 40 mm',
           'Block/ Brick',
           'RCC - Design Mix-7.5 m20,m25',
-          'Ceiling Height= 11 feet'
+          'Ceiling Height= 11 feet',
+          'Staircase, Balcony, Roof - Railing, Elevation',
+          'SS malt finish',
+          'False Ceiling',
+          'TV wall panelling',
+          'Wall moulding desing',
+          'Footing - Waterproofing',
         ]
       },
       {
         title: 'Kitchen',
         details: [
-          'Main Sink Faucet - upto 4000/-',
-          'Kitchen Sink of S.S. granite finish, modular Kitchen',
-          'Water Proofing Nitoproof 600 PF Epoxy'
+          'Main Sink Faucet - upto 10000/-',
+          'Kitchen top granite finish, modular Kitchen',
+          'SS Kitchen ',
+          'Chimney, Fridge',
+          'Wall tiles ceramic upto Rs 100 per sqft',
+          'Dado tiles upto Rs 125 per sqft',
+          'Sink area - Waterproofing',
         ]
       },
       {
         title: 'Bathroom',
         details: [
           'Nitoproof 600 PF Epoxy',
-          'Sanitarycrare & CPVC Fittings up to Rs 80000/- Kohler, Jaguar make',
-          'CPVC pepe - APL Apollo',
+          'Sanitary wear & CPVC Fittings up to Rs 80000/-',
+          'CPVC fitting - Astral/Supreme',
           'Doors - waterproof flush doors',
-          'Mirror, soap dish, Towel Rail, of Rs - 10000/- geyser. 20 LTS'
+          'Mirror, soap dish, Towel Rail, of Rs - 10000/- geyser. 20 LTS',
+          'Sunken Waterproofing - Agent (Pidilite/URP)',
+          'Heat grill',
+          'Glass partition (Water guard)',
+          'Ventilation fan 3"',
+          'Overhead water tank SS',
         ]
       },
       {
         title: 'Doors & Windows',
         details: [
-          'UPVC windows with (3 Track). of fenesta make',
-          'Main doors - Teak wood frame',
-          'Teak Doors (5x3.5)',
-          'Internal doors - flush doors, with laminates, frame Sal or  Marandi 4x3)'
+          'UPVC windows with (3 Track), Fix window - glass 10mm laminated toughened glass',
+          'Main doors - Nagpur seasoned teak',
+          'Internal doors - flush doors with veneer, frame Sal or  Marandi 4x3)',
+          'Door frame - teak wood'
         ]
       },
       {
         title: 'Painting',
         details: [
-          '3K putty',
-          'Royale Luxury, Emulsion or similar',
-          'External Asian Primer, Apex ultima. External Emulsion Paint or similar'
+          'Internal - Wall POP, Primer, Chaak Primer, Royal Emulsion for Ceiling, Velvet touch for walls (Deluxe/Berger)',
+          'External - JK putty, Primer waterproofing, Weather shield paint, Texture paint as decided',
 
         ]
       },
       {
         title: 'Flooring',
         details: [
-          'Living+ Dinning Flooring - Tiles or Granitel Or Marble of Rs- 180/SQFT or Italian Tiles',
-          'Room + Kitchen - Tiles or Granite or marbles. of Rs. 140/SQFT'
+          'Italian marble for rooms and living area',
+          'Kitchen - Ceramic tiles for wall , dado',
+          'Kitchen floor - Italian tiles',
+          'Fair area - Moraccon tiles',
+          'Balcony and Parking - Antiskid tiles',
+          'Roof flooring - Moraccon tiles',
+          'Garden area and Parking - Paver block, Kota stone',
+          'Water proofing - Roof and Balcony',
+
         ]
       },
       {
         title: 'Electrical',
         details: [
-          'Overhead water tank. 2000 Lt',
-          'Wires - Fireproof - Finolex',
-          'Switches & sockets Legrand, Schneider, Havells',
-          'UPS wiring, charging 1 EV charging Point at G.F'
+          'Wires (FRLS)',
+          'Switches & sockets COMA, Schneider, Havells',
+          'UPS wiring, car charging point',
+          'Condiut - AKG',
+          'Lights, Fans, Chandeliers, Earthing'
+        ]
+      },
+      {
+        title: 'Smart Homes',
+        details: [
+          'Wi-Fi CAT-6',
+          'CCTV Camera',
+          'Main Gate lock - Smart Lock',
+          'Smart switches paired with Alexa',
+          'Smart locks (Bluetooth, wifi)',
+          'Centralized RO fitting',
+          'Shoftner',
+          'Heat pump',
+          'SS overhead water tank'
         ]
       }
     ]
